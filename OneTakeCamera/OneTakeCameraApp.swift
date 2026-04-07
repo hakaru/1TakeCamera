fd56309a209f8541f83463308d1414337392a048
Hakaru Hirose <hakaru@M4Air.local>
2026-04-07 18:56:40 +0900

    feat(camera): set display name to '1Take Camera' (v0.1 Task 8)

---
+++ OneTakeCamera/OneTakeCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneTakeCameraApp.swift
+//  OneTakeCamera
+//
+//  Created by Hakaru Hirose on 2026/04/07.
+//
+
+import SwiftUI
+
+@main
+struct OneTakeCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
